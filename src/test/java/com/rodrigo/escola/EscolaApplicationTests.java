c43d894ccabee65200b77add1a0cf8975617828d
RodrigoMCarvalho <rodrigo_moreira84@hotmail.com>
2020-06-25 19:26:53 -0300

    first commit

---
+++ src/test/java/com/rodrigo/escola/EscolaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.escola;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
